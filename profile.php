60545cf6c3030036524c16bc4d3cc388cc95285c
yoni1243 <yo6460464@gmail.com>
2023-12-18 15:50:27 +0300

    last 4 olla

---
+++ profile.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+
+
+    <head>
+        <title>My Profile</title>
+        <link rel="stylesheet" href="C:\Users\bingx\Downloads\bootstrap-5.2.3-dist\bootstrap-5.2.3-dist\css\bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans|Roboto+Mono&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+
+    <body>
+
+      <nav class="navbar navbar-expand-md " >
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="main-navigation ">
+            <ul class="navbar-nav" >
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+      <div class="container">
+
+        
+                <div class="container d-flex justify-content-center align-items-center" style="height: 300px;">
+                  <div class="row">
+                    <div class="col-sm-12 text-center">
+
+                    <div class="intro">
+                        <img class="profile-picture" src="images/man-user-color-icon.png" 
+                        width="150" height="150"
+                        >
+                        <h1>Yonatan T.</h1>
+                        <h3>Web Developer At MOND</h3>
+                        <p> <i>  "fly above negativity!!!" </i> </p>
+                    </div>
+
+                    </div>
+                  </div>
+               </div>
+
+            <hr/>
+
+         <div class="row">            
+              <div class="col-md-4 ">
+                    <h3>I am</h3>
+                    <ul>
+                    <li>An engineer</li>
+                    <li>A reader</li>
+                    <li>A soldier</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 ">
+                    <h3>I like to</h3>
+                    <ul>
+                    <li>Search new technology</li>
+                    <li>Play game</li>
+                    <li>Coding</li>
+                    </ul>
+                </div>
+            </div>
+
+                <h4 class="projects-heading">My Projects</h4>
+                  <div class="row">
+                    <div class="col-md-6 col-md-6 ">
+                      <div class="projects-grid">    
+                       <div class="project-image-wrapper">
+                         
+                         <img class="project-image" src="images/Inventory-Management.jpg" 
+                         width="300" height="250"
+                         />
+                       </div>
+                     </div> 
+                    </div> 
+                     <div class="col-md-6 col-md-6 ">
+                         <div class="project-image-wrapper">
+                           
+                         <img class="project-image" src="images/HM.jpg" 
+                         width="300" height="250"
+                         />
+                         </div>
+                     </div>
+                     <div class="col-md-6 col-md-6 ">
+                         <div class="project-image-wrapper">
+                      
+                         <img class="project-image" src="images/HM.jpg" 
+                         width="300" height="250"
+                         />
+                         </div>
+                     </div>
+                     <div class="col-md-6 col-md-6 ">
+                       <div class="project-image-wrapper">
+                        
+                       <img class="project-image" src="images/court-mediation-judge-technology-.jpg"
+                       width="300" height="250"
+                       />
+                       </div>
+                     </div>
+                         
+ 
+                     </div>
+                  </div>
+                   
+                  <div class="row">            
+                    <div class="col-md-6 ">
+
+                            <div>
+                              <h3>Links</h3>
+                              <ul>
+                                <li>
+                                  <a href="https://github.com/<user>">Github</a>
+                                </li>
+                                <li>
+                                  <a href="https://twitter.com/<user>">Twitter</a>
+                                </li>
+                                <li>
+                                  <a href="https://linkedin.com/in/<user>">Linkedin</a>
+                                </li>
+                              </ul>
+                          </div>
+                     </div>
+                   <div class="col-md-6 ">
+                        <div>
+                          <form action="#">
+                            <label for="email">
+                              <h4>Email:</h4>
+                              <input type="email" id="email" placeholder="Enter your email" /> 
+                            </label>
+                            <label for="message">
+                              <h4>Message:</h4>
+                              <textarea id="message">Your Message</textarea>
+                            </label>
+                            <div class="submit-button-wrapper">
+                              <input type="submit" valid="Send Message"/>
+                            </div>
+                          
+                          </form>
+                        </div>
+                    </div>
+
+                    </div>
+                 </div>
+
+                        
+              <div class="row"> 
+                <footer class="page-footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 col-md-8 col-sm-12">
+                                <h6 class="text-uppercase font-weight-bold">Additional Information</h6>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Quisque interdum quam odio, quis placerat ante luctus eu.
+                                     Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Quisque interdum quam odio, quis placerat ante luctus eu. 
+                                    Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+                        </div>
+                        <div class="col-lg-4 col-md-4 col-sm-12">
+                            <h6 class="text-uppercase font-weight-bold">Contact</h6>
+                            <p>1640 Riverside Drive, Hill Valley, California
+                            <br/>info@mywebsite.com
+                            <br/>+ 01 234 567 88
+                            <br/>+ 01 234 567 89</p>
+                        </div>
+                    <div>
+                    <div class="footer-copyright text-center">© 2023 Copyright: MyWebsite.com</div>
+                </footer>
+              </div>
+      </div>
+
+    </body>
+</html>
